fb1247a5b4f21010017fd11d2e29035811f32d2b
Amin Dannak <amin.dannak@gmail.com>
2020-07-11 07:33:33 +0430

    initial commit - just a hello world

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
